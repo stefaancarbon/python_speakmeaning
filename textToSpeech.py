e07e9ad246bc0bc17757c5224232e8c0be778520
Stefaan Carbon <stefaan.carbon@gmail.com>
2026-03-11 17:25:29 -0400

    Initial Python commit

---
+++ textToSpeech.py
@@ -0,0 +1,9 @@
+from gtts import gTTS
+import os
+
+mytext = 'Welcome to GeeksforGeeks Joe!'
+language = 'en'
+
+myobj = gTTS(text=mytext, lang=language, slow=False)
+myobj.save("welcome.mp3")
+os.system("start welcome.mp3")
